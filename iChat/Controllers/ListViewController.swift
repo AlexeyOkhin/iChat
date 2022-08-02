21141cbe4bb7ea5c54e90916af0b58069378d559
Alexey <k-i-ll-e-r@list.ru>
2022-08-02 12:55:34 +0500

    prepare tabBarController for collectionView screen

---
+++ iChat/Controllers/ListViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ListViewController.swift
+//  iChat
+//
+//  Created by Djinsolobzik on 01.08.2022.
+//
+
+import UIKit
+import SwiftUI
+
+class ListViewController: UIViewController {
+    
+    var collectionView: UICollectionView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        setupCollectionView()
+
+    }
+    
+}
+//MARK: - setup collectionView
+extension ListViewController {
+    private func setupCollectionView() {
+        collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        //collectionView.backgroundColor = .white
+        self.view.addSubview(collectionView)
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellid")
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+    }
+}
+
+//MARK: - setting delegate datasourse
+
+extension ListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+
+
+
+//MARK: - SwiftUi Canvas
+import SwiftUI
+
+struct ListControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all).previewInterfaceOrientation(.portrait)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        let viewController = ListViewController()
+        func makeUIViewController(context: Context) -> ListViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+            
+        }
+    }
+}
---
+++ iChat/Controllers/PeopleViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PeopleViewController.swift
+//  iChat
+//
+//  Created by Djinsolobzik on 01.08.2022.
+//
+
+import UIKit
+
+class PeopleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .orange
+    }
+
+
+}
